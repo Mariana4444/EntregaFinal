51f0055865b1692a5002d35910f596648cb210ae
Mariana <marianagarcia4444@gmail.com>
2024-02-25 19:01:15 -0300

    Entrega final

---
+++ constructor.js
@@ -0,0 +1,45 @@
+// se utilizo para crear el archivo usuarios.json
+class Usuario {
+    constructor(nombre, apellido, cedula, edad, telefono) {
+      this.nombre = nombre;
+      this.apellido = apellido;
+      this.cedula = cedula;
+      this.edad = edad;
+      this.telefono = telefono;
+    }
+  }
+  
+  let usuario1 = new Usuario("Mariana", "Garcia", 41456958, 40, 92598756);
+  let usuario2 = new Usuario("Joel", "Cabrera", 46327892, 42, 96567456);
+  let usuario3 = new Usuario("Maria", "Silveira", 9865324, 24, 94562315);
+  let usuario4 = new Usuario("Luisa", "Altamiranda", 19872314, 70, 94712698);
+  let usuario5 = new Usuario("Sergio", "Muniz", 3621987, 85, 36521456);
+  let usuario6 = new Usuario("Noire", "Migues", 24893569, 56, 98320146);
+  let usuario7 = new Usuario("Odin", "Altamiranda", 62318974, 25, 96741836);
+  let usuario8 = new Usuario("Simba", "Gutierrez", 38974125, 64, 93823789);
+  let usuario9 = new Usuario("Claudia", "Mendez", 61473968, 36, 96378116);
+  let usuario10 = new Usuario("Andres", "Lima", 21476650, 56, 93874123);
+  let usuario11 = new Usuario("Mariana", "Lima", 21476650, 56, 93874123);
+  let usuario12 = new Usuario("Joel", "Rodrigues", 21476650, 56, 93874123);
+  let usuario13 = new Usuario("Noire", "Bertotti", 21476650, 56, 93874123);
+  let usuario14 = new Usuario("Odin", "Fernandez", 21476650, 56, 93874123);
+  let usuario15 = new Usuario("Simba", "Secco", 21476650, 56, 93874123);
+  
+  let lista = [
+    usuario1,
+    usuario2,
+    usuario3,
+    usuario4,
+    usuario5,
+    usuario6,
+    usuario7,
+    usuario8,
+    usuario9,
+    usuario10,
+    usuario11,
+    usuario12,
+    usuario13,
+    usuario14,
+    usuario15,
+  ];
+  
--- main.js
+++ main.js
@@ -1,57 +1,9 @@
-/* constructor*/
 
-class Usuario {
-  constructor(nombre, apellido, cedula, edad, telefono) {
-    this.nombre = nombre;
-    this.apellido = apellido;
-    this.cedula = cedula;
-    this.edad = edad;
-    this.telefono = telefono;
-  }
-}
 
-let usuario1 = new Usuario("Mariana", "Garcia", 41456958, 40, 92598756);
-let usuario2 = new Usuario("Joel", "Cabrera", 46327892, 42, 96567456);
-let usuario3 = new Usuario("Maria", "Silveira", 9865324, 24, 94562315);
-let usuario4 = new Usuario("Luisa", "Altamiranda", 19872314, 70, 94712698);
-let usuario5 = new Usuario("Sergio", "Muniz", 3621987, 85, 36521456);
-let usuario6 = new Usuario("Noire", "Migues", 24893569, 56, 98320146);
-let usuario7 = new Usuario("Odin", "Altamiranda", 62318974, 25, 96741836);
-let usuario8 = new Usuario("Simba", "Gutierrez", 38974125, 64, 93823789);
-let usuario9 = new Usuario("Claudia", "Mendez", 61473968, 36, 96378116);
-let usuario10 = new Usuario("Andres", "Lima", 21476650, 56, 93874123);
-let usuario11 = new Usuario("Mariana", "Lima", 21476650, 56, 93874123);
-let usuario12 = new Usuario("Joel", "Rodrigues", 21476650, 56, 93874123);
-let usuario13 = new Usuario("Noire", "Bertotti", 21476650, 56, 93874123);
-let usuario14 = new Usuario("Odin", "Fernandez", 21476650, 56, 93874123);
-let usuario15 = new Usuario("Simba", "Secco", 21476650, 56, 93874123);
-
-let lista = [
-  usuario1,
-  usuario2,
-  usuario3,
-  usuario4,
-  usuario5,
-  usuario6,
-  usuario7,
-  usuario8,
-  usuario9,
-  usuario10,
-  usuario11,
-  usuario12,
-  usuario13,
-  usuario14,
-  usuario15,
-];
+let lista = []
 
 function nombre() {
-  let nombreUsuario = JSON.parse(localStorage.getItem("usuario"));
-  if (!nombreUsuario) {
-    nombreUsuario = prompt("ingrese su nombre");
-    console.log(nombreUsuario);
-  }
-
-  if (nombreUsuario !== null) {
+  if (nombreUsuario !== null && lista.length>0) {
     let usuarioFiltrado = lista.filter((item) => item.nombre === nombreUsuario);
     console.log(usuarioFiltrado);
     if (usuarioFiltrado.length > 0) {
@@ -85,9 +37,79 @@ function nombre() {
       // guardamos el usuario formateado
       localStorage.setItem("usuario", usuarioConvertido);
     } else {
-      alert("Usuario no encontrado");
+      Swal.fire({
+        icon: "error",
+        title: "Oops...",
+        text: "Usuario no encontrado!!!",
+        color: "white",
+      });
     }
   }
 }
 
-nombre();
+
+
+function getLista() {
+  axios
+    .get("usuarios.json")
+    .then((response) => {
+      const data = response.data;
+      lista = [...data];  
+      console.log("Datos obtenidos:", lista);
+      if(nombreUsuario){
+        nombre()
+      }
+    })
+    .catch((error) => {
+      console.error("Error en la solicitud Axios:", error);
+    });
+}
+
+
+getLista();
+
+
+let nombreUsuario = JSON.parse(localStorage.getItem("usuario"));
+console.log(nombreUsuario)
+
+let ingreseNombre = document.getElementById("formulario");
+// ver sustitur el prompt por un botton
+let buscador = document.getElementById("buttonUno");
+
+buscador.addEventListener("click", function (e) {
+  e.preventDefault();
+  
+  if (nombreUsuario) {
+    Swal.fire({
+      title: "Desea confirmar el nombre?",
+
+      icon: "question",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Si,quiero",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        Swal.fire({
+          title: "Exito",
+          text: "Se realizado la busqueda con exito",
+          color: "white",
+          icon: "success",
+        });
+        nombre();
+      }
+    });
+  } else {
+    console.log("Ingrese su nombre");
+  }
+});
+
+ingreseNombre.addEventListener("change", function () {
+  nombreUsuario = ingreseNombre.value;
+  console.log(nombreUsuario);
+  console.log(ingreseNombre.value);
+});
+
+if (nombreUsuario && lista.length>0){
+  nombre();
+}
